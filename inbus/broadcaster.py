67ac22db51b24e53312a155298fa316bb0dd8ee8
Maarten <mlos@gmx.com>
2017-11-01 22:17:48 +0100

    Implement broadcast and subscriber

--- inbus/broadcaster.py
+++ inbus/broadcaster.py
@@ -8,9 +8,10 @@ class Broadcaster(object):
         if subscriber.want_to_subscribe():
             self._subscribers.append(subscriber)
         elif subscriber.want_to_unsubscribe():
-            self._subscribers = [s for s in self._subscribers if s.id() != subscriber.id()]
+            self._subscribers = [s for s in self._subscribers if s.address() != subscriber.address()]
             
     def broadcast_publisher(self, publisher):
-        for subscriber in self._subscribers:
-            pass
-            
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        addresses = [s.address() for s in self._subscribers if s.application_interest() == publisher.application()]
+        for addr in addresses:
+            mysock.sendto("SERVER", addr)
