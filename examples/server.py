e4f6dcea6885fb9a5a66899abaa7a09cc0fd47cb
Maarten <mlos@gmx.com>
2017-11-05 15:35:36 +0100

    Added examples

---
+++ examples/server.py
@@ -0,0 +1,6 @@
+
+import inbus
+
+inbus = Inbus()
+
+inbus.run()
---
+++ server/inbus.py
@@ -0,0 +1,41 @@
+import socket
+
+from broadcaster import Broadcaster
+from incoming_message import IncomingMessage
+
+class Inbus(object):
+
+    def __init__(self, address=(127.0.0.1, 7222), buffer_size=65536):
+        self._address = address
+        self._buffer_size = buffer_size
+        self._socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self._socket.bind(address)
+        self._broadcaster = Broadcaster()
+
+    def run(self):
+        while True:
+            self._listen_and_process_message()
+
+
+    def _listen_and_process_message(self):
+        data, addr = self._socket.recvfrom(self._buffer_size)
+        incoming_message = None
+
+        try:
+            incoming_message = IncomingMessage(data)
+        except:
+            continue;
+
+        print "A"
+
+        subscriber = incoming_message.to_subscriber()
+        if subscriber:
+            bc.manage_subscriber(subscriber)
+            del subscriber
+        else:
+            publisher = incoming_message.to_publisher()
+            if publisher:
+                bc.broadcast_publisher(publisher)
+                del publisher
+
+        del incoming_message
