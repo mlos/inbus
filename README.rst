46f6a11def98e68de41ac18efab8b4c2e96cdea6
Maarten <mlos@gmx.com>
2017-11-08 21:36:00 +0100

    Prepared for packaging

--- README.rst
+++ README.rst
@@ -1,3 +1,27 @@
+inbus **|** 
+
+Inbus
+=========
+
+.. image:: https://travis-ci.org/mlos/inbus.svg?branch=master
+   :target: https://travis-ci.org/mlos/inbus
+
+
+**inbus** stands for INconnu message BUS and is targeted at small
+devices running a limited number of applications exchanging small
+messages. It has a single goal: connectionless brokering of messages
+between one or more publishers and one or more subscribers.
+
+* Central broker
+* Does not try to be all things to all people
+* Connectionless (UDP based): No message delivery guaranteed
+* No flow control or session management 
+* Simple JSON based protocol
+
+License
+-------
+BSD 2-Clause License
+
 Copyright (c) 2017 Maarten Los
 All rights reserved.
 
---
+++ setup.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+from io import open
+from setuptools import setup
+
+
+def read_file(fname, encoding='utf-8'):
+    with open(os.path.join(os.path.dirname(__file__), fname), encoding=encoding) as r:
+        return r.read()
+
+
+README = read_file("README.rst")
+CHANGES = read_file("CHANGES.rst")
+version = read_file("VERSION.txt").strip()
+
+needs_pytest = {'pytest', 'test', 'ptr'}.intersection(sys.argv)
+pytest_runner = ['pytest-runner'] if needs_pytest else []
+test_deps = [
+    'mock;python_version<"3.3"',
+    "pytest>=3.1",
+    "pytest-cov"
+]
+
+install_deps = [
+    'json'
+]
+
+setup(
+    name="inbus",
+    version=version,
+    author="Maarten Los",
+    author_email="mlos@gmx.com",
+    description=("Simple, UDP-based message bus"),
+    long_description="\n\n".join([README, CHANGES]),
+    license="BSD 2-Clause license",
+    keywords="message bus broker publisher subscriber pub sub",
+    url="https://github.com/mlos/inbus",
+    download_url="https://github.com/mlos/inbus/tarball/" + version,
+    namespace_packages=["inbus"],
+    packages=["inbus"],
+    install_requires=install_deps,
+    setup_requires=pytest_runner,
+    tests_require=test_deps,
+    extras_require={
+        'docs': [
+            'sphinx>=1.5.1'
+        ],
+        'test': test_deps,
+        'qa': [
+            'flake8',
+            'rstcheck'
+        ]
+    },
+    zip_safe=False,
+    classifiers=[
+        "License :: OSI Approved :: BSD License",
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: Developers",
+        "Topic :: Education",
+        "Topic :: System :: Networking",
+        "Topic :: System :: Distributed Computing",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6"
+    ]
+)
