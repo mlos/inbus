2e60efe7ba729e4debcc7478c34127c59a1de8b8
Maarten <mlos@gmx.com>
2017-12-11 22:18:57 +0100

    Implemented in terms of InbusMethodObserver

--- inbus/server/broadcaster.py
+++ inbus/server/broadcaster.py
@@ -7,20 +7,27 @@
 class Broadcaster(object):
 
     def __init__(self, registry, message_sender):
+        if registry is None:
+            raise AttributeError
+
+        if message_sender is None:
+            raise AttributeError
+
         self._registry = registry
         self._message_sender = message_sender
+
 
     def subscribe(self, address, application):
         pass
 
+
     def unsubscribe(self, address, application):
         pass
 
-    def publish(self, address, application, payload):
+
+    def publish(self, application, payload):
         subscribers = self._registry.subscribers()
-        addresses = [tuple(address) for s in subscribers
-                     if s.application() == application[0]]
-        print application[0]
+        addresses = [tuple(s[0]) for s in subscribers if s[1] == application]
 
         for addr in addresses:
             self._message_sender.send(addr, application, payload)
